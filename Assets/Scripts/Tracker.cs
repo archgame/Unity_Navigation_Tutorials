6ce05d8793dcf7ebfb7885c6b96f5687ff353ff2
archgame <matthewjosephconway@gmail.com>
2019-09-28 11:47:36 -0700

    added raycast with target movement

--- Assets/Scripts/Tracker.cs
+++ Assets/Scripts/Tracker.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Tracker : MonoBehaviour
 {
     public bool run = false;
+    public GameObject target;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,29 @@ public class Tracker : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             Debug.Log("Left Click");
+
+            //creates a ray from the camera
+            Vector3 screenPoint = Input.mousePosition;
+            Debug.Log("screenPoint: " + screenPoint);
+            Ray ray = Camera.main.ScreenPointToRay(screenPoint);
+            float length = 200;
+            Debug.DrawRay(ray.origin, ray.direction * length,Color.red,2);
+
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(ray.origin, ray.direction, Mathf.Infinity); //find all gameobjects
+            Debug.Log("hits.Length: " + hits.Length);
+            foreach(RaycastHit hit in hits)
+            {
+                Debug.Log("name: " + hit.transform.gameObject.name);
+                if(hit.transform.gameObject.name == "ground")
+                {
+                    Debug.Log("'ground' hit!");
+                    Debug.Log("hit.point: " + hit.point);
+                    Debug.DrawRay(hit.point, Vector3.up * 50, Color.cyan, 2);
+                    target.transform.position = hit.point;
+                }
+            }
+
         }
 
         //Right mouse button click example
