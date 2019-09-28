97418e58d2045439fafde71855483f16effd9732
archgame <matthewjosephconway@gmail.com>
2019-09-28 12:10:12 -0700

    Smoother Camera Position

--- Assets/Scripts/Tracker.cs
+++ Assets/Scripts/Tracker.cs
@@ -6,16 +6,32 @@ public class Tracker : MonoBehaviour
 {
     public bool run = false;
     public GameObject target;
+    public float smoothSpeed = 0.125f;
+    private Vector3 newPosition = Vector3.zero;
+    //float example = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start");
+        //Debug.Log("Start");
+        newPosition = Camera.main.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //float example = 0;
+        //Debug.Log("before example: " + example);
+        //example = Mathf.Lerp(example, 10, 0.5f);
+        //Debug.Log("after example: " + example);
+
+        //smoothly move camera
+        Vector3 cameraPosition = Camera.main.transform.position;
+        newPosition.y = cameraPosition.y;
+        Vector3 smoothedPosition = Vector3.Lerp(cameraPosition, newPosition, smoothSpeed);
+        Camera.main.transform.position = smoothedPosition;
+
+        Vector3 clickPosition = Vector3.zero;
         //Left mouse button click example
         if (Input.GetMouseButtonUp(0))
         {
@@ -48,27 +64,24 @@ public class Tracker : MonoBehaviour
         //Right mouse button click example
         if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log("Right Click");
+            //Debug.Log("Right Click");
 
             //creates a ray from the camera
             Vector3 screenPoint = Input.mousePosition;
-            Debug.Log("screenPoint: " + screenPoint);
+            //Debug.Log("screenPoint: " + screenPoint);
             Ray ray = Camera.main.ScreenPointToRay(screenPoint);
             float length = 200;
             Debug.DrawRay(ray.origin, ray.direction * length, Color.yellow, 2);
 
             RaycastHit[] hits;
             hits = Physics.RaycastAll(ray.origin, ray.direction, Mathf.Infinity); //find all gameobjects
-            Debug.Log("hits.Length: " + hits.Length);
+            //Debug.Log("hits.Length: " + hits.Length);
             foreach (RaycastHit hit in hits)
             {
-                Debug.Log("name: " + hit.transform.gameObject.name);
+                //Debug.Log("name: " + hit.transform.gameObject.name);
                 if (hit.transform.gameObject.name == "ground")
                 {
-                    float y = Camera.main.transform.position.y;
-                    float x = hit.point.x;
-                    float z = hit.point.z;
-                    Camera.main.transform.position = new Vector3(x, y, z);
+                    newPosition = hit.point;
                 }
             }
         }
