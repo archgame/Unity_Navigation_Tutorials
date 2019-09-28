33dba9726437848d48b97b3f335f34e4f51aff08
archgame <matthewjosephconway@gmail.com>
2019-09-28 13:31:34 -0700

    Sample Agent added

---
+++ Assets/Scripts/SampleAgent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SampleAgent : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>(); //set the agent variable to this game object's navmesh
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position); //each frame set the agent's destination to the target position
+    }
+}
