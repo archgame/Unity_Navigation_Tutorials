07d0059341dd771e3208d9c95aa38a18fe5a58bb
archgame <matthewjosephconway@gmail.com>
2019-10-05 11:17:33 -0700

    Automatically assign targets by tag

--- Assets/Scripts/SampleAgent.cs
+++ Assets/Scripts/SampleAgent.cs
@@ -7,27 +7,30 @@ public class SampleAgent : MonoBehaviour
 {
     Transform target;
     NavMeshAgent agent;
-    public Transform[] targets;
+    public GameObject[] targets;
     public float changeTargetDistance = 3;
     int t;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //grab targets using tags
+        if (targets == null || targets.Length == 0)
+        {
+            targets = GameObject.FindGameObjectsWithTag("Target");
+        }
+        Debug.Log(this.name + " has " + targets.Length + "Targets");
 
         agent = GetComponent<NavMeshAgent>(); //set the agent variable to this game object's navmesh
         t = 0;
-        target = targets[t];
+        target = targets[t].transform;
         agent.SetDestination(target.position);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {       
         float distanceToTarget = Vector3.Distance(agent.transform.position, target.position);
-        //Debug.Log("distanceToTarget: " + distanceToTarget);
         if (changeTargetDistance > distanceToTarget)
         {
             t++;
@@ -35,9 +38,8 @@ public class SampleAgent : MonoBehaviour
             {
                 t = 0;
             }
-
             Debug.Log(this.name + " Change Target: " + t);
-            target = targets[t];
+            target = targets[t].transform;
             agent.SetDestination(target.position); //each frame set the agent's destination to the target position
         }        
     }
